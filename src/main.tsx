19818531fb57d6f0e8363dfd412c18a8fc99e618
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-23 18:59:06 +0300

    feat: 🎸 add aliases path's and change dir lib

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "../lib/store/store.ts";
+import { store } from "@lib/store/store.ts";
 import App from "./App.tsx";
 import "./index.css";
 
