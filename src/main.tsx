5457ffec3f19c984bc40cf626bf4daaf06a0eb15
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-24 19:17:51 +0300

    feat: 🎸 add react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { store } from "./lib/store/store.ts";
-import App from "./App.tsx";
 import "./index.css";
+import { HomePage } from "./pages/HomePage/HomePage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
