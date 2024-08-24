2b4f4eb748b261dbeead68363f23d64b12d0f08e
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-24 19:21:05 +0300

    fix: 🐛 change default html tag to react router dom tag

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,15 @@
 import { IoLogoTux } from "react-icons/io";
 import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <div>
       <div className={styles.header}>
         <div className={styles["header__logo"]}>
-          <a href="/">
+          <Link to="/">
             <IoLogoTux size={50} />
-          </a>
+          </Link>
         </div>
         <div className={styles["header__nav"]}></div>
       </div>
