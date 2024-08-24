812d1df315ba437e85834def6a3e55a0b9e23afc
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-24 20:29:03 +0300

    feat: 🎸 connect slice to store
    ✅ Closes: g

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import userReducer from './slice/userSlice';
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    users: userReducer
+  },
 });
 
 
-export type AppStore = ReturnType<typeof store>;
-export type RootState = ReturnType<AppStore['getState']>;
-export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
