56c40aef674152527ff3fc67a0b08174180066ad
iwakura-dev <kozhemyakokirill@gmail.com>
2024-08-24 20:28:36 +0300

    fix: 🐛 rechange type for input

--- src/types/interface.types.ts
+++ src/types/interface.types.ts
@@ -1,7 +1,5 @@
 export interface InputProps {
-  type: string;
   placeholder?: string;
-  value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  className?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
